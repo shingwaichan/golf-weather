53e707b8fb291656f6839c0e0c0469b33564635e
dhiru <dhiru.pandey@oracle.com>
2016-12-13 18:35:06 -0800

    Updated code with lat, long for nearby golf courses

--- zipcode/zipcodeService.js
+++ zipcode/zipcodeService.js
@@ -7,7 +7,6 @@ var googleApiKey = "AIzaSyAWb5olGhXydFCpRmGhTJGOCP8yKsN6DLY";
 
 // var zipCode = "94539";
 
-console.log("Coming here 1");
 // get is a simple wrapper for request()
 // which sets the http method to GET
 
@@ -23,14 +22,42 @@ var options = {
 getLatLongForZipcode(zipCode, function(p_id) {
     console.log("Got Lat, Long:", p_id);
 });
+
+getGolfCoursesForZipcode(94539, function(ll) {
+    console.log("========================");
+    console.log(ll);
+});
 */
+
+
+function getGolfCoursesForZipcode(zip, callback) {
+    getLatLongForZipcode(zip, function(ll) {
+        var lat = JSON.stringify(ll.lat);
+        var lng = JSON.stringify(ll.lng);
+        var u = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat +
+                "," + lng + "&radius=50000&keyword=Golf+Course&key=" + googleApiKey;
+
+        getJSONData(u, function(data) {
+            var results = data.results;
+            var ans = [];
+            for (var i = 0; i < results.length; i++) {
+                var jsonData = {};
+                jsonData["name"] = results[i].name;
+                jsonData["lat"] = results[i].geometry.location.lat;
+                jsonData["lng"] = results[i].geometry.location.lng;
+                ans.push(jsonData);
+            }
+            callback(ans);
+        });
+    });
+}
 
 function getLatLongForZipcode(zip, callback) {
     var u = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + zip +"&" + "key=" + googleApiKey;
     console.log("URL --- " + u);
     getJSONData(u, function(data) {
         var p = data.results[0].geometry.location;
-        console.log("location:", p);
+//        console.log("location:", p);
         callback(p);
     });
 }
@@ -68,8 +95,8 @@ function handleRequest(request, response){
     var queryObj = url.parse(request.url,true).query;
     var zipCode = queryObj.zipcode;
     if (typeof zipCode !== 'undefined' && zipCode) {
-        getLatLongForZipcode(zipCode, function(ll) {
-            console.log("Got Lat, Long:", ll);
+        getGolfCoursesForZipcode(zipCode, function(ll) {
+//            console.log("Got Lat, Long:", ll);
             response.write(JSON.stringify(ll));
             response.end();
         });
